ed73ff186db08c98eda3259ccf4fd7c37c65f172
nathan29849 <phs960418@gmail.com>
2023-02-12 16:30:46 +0900

    서블릿 요청처리

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,13 +1,36 @@
 package com.example.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 public class TobySpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TobySpringbootApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// 익명 클래스
+		WebServer webServer = serverFactory.getWebServer(servletContext ->
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					// Request : query string
+					String name = req.getParameter("name");
+
+					// Response : Status Code, Content-Type, Body
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
+				}
+			}).addMapping("/hello"));
+		webServer.start();
 	}
 
 }
