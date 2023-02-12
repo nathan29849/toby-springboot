987268b2c7a0b250b5699d3b315d6a760102f452
nathan29849 <phs960418@gmail.com>
2023-02-13 00:19:36 +0900

    @ComponentScan

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -5,24 +5,14 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class TobySpringbootApplication {
-
-	// 팩토리 메서드를 통해 빈 오브젝트를 만든다.
-	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
 
 	public static void main(String[] args) {
 
