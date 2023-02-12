5fe10f4011fcd8df02d6443fa326f0391ed1c6d2
nathan29849 <phs960418@gmail.com>
2023-02-12 23:28:03 +0900

    어노테이션 매핑 정보 사용

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,24 +1,17 @@
 package com.example.tobyspringboot;
 
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TobySpringbootApplication {
 
 	public static void main(String[] args) {
 
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh(); // applicationContext 가 Bean Object를 만들어주는 메서드
@@ -26,30 +19,9 @@ public class TobySpringbootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
-			servletContext.addServlet("front-controller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-					// 인증, 보안, 다국ㄱ어, 공통 기능
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-
-						// Request : query string
-						String name = req.getParameter("name");
-
-						// handler Mapping
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						// Response : Status Code, Content-Type, Body
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*"));
+			servletContext.addServlet("dispatcherServlet",
+				new DispatcherServlet(applicationContext))
+				.addMapping("/*"));
 		webServer.start();
 	}
 
