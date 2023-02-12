6d8badc9fd4731977195e069dc74fd8125d9ffac
nathan29849 <phs960418@gmail.com>
2023-02-13 00:06:34 +0900

    자바 코드 구성 정보 사용

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -4,14 +4,29 @@ package com.example.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class TobySpringbootApplication {
+
+	// 팩토리 메서드를 통해 빈 오브젝트를 만든다.
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -23,8 +38,7 @@ public class TobySpringbootApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(TobySpringbootApplication.class);
 		applicationContext.refresh(); // applicationContext 가 Bean Object를 만들어주는 메서드
 
 
